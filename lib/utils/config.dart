caca35e225cf6cc36de2674003e6b72047245f4b
YoussefToumi <yousseftoumi42@gmail.com>
2022-08-10 16:24:34 +0100

    AlerDialog

--- lib/utils/config.dart
+++ lib/utils/config.dart
@@ -12,3 +12,44 @@ const kTextColor = Color.fromRGBO(56, 182, 255, 1);
 const kTextLightColor = Color.fromARGB(255, 171, 185, 196);
 
 const kDefaultPaddin = 20.0;
+
+enum DialogsAction { Oui, Annuler }
+
+class AlertDialogs {
+  static Future<DialogsAction> yesCancelDialog(
+    BuildContext context,
+    String title,
+    String body,
+  ) async {
+    final action = await showDialog(
+        context: context,
+        barrierDismissible: false,
+        builder: (BuildContext context) {
+          return AlertDialog(
+            shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10.0)),
+            title: Text(title),
+            content: Text(body),
+            actions: <Widget>[
+              ElevatedButton(
+                onPressed: () =>
+                    Navigator.of(context).pop(DialogsAction.Annuler),
+                child: Text(
+                  'Cancel',
+                  style: TextStyle(
+                       fontWeight: FontWeight.bold),
+                ),
+              ),
+              ElevatedButton(
+                onPressed: () => Navigator.of(context).pop(DialogsAction.Oui),
+                child: Text(
+                  'Confirm',
+                  style: TextStyle(
+                       fontWeight: FontWeight.w700),
+                ),
+              )
+            ],
+          );
+        },);
+        return (action != null) ? action : DialogsAction.Annuler;
+  }
+}
