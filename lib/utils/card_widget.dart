43c4afbb34f06a8390ef3a333c837a995edae304
YoussefToumi <yousseftoumi42@gmail.com>
2022-08-12 15:42:21 +0100

    Bike details

--- lib/utils/card_widget.dart
+++ lib/utils/card_widget.dart
@@ -1,4 +1,6 @@
+import 'package:bikeapp_v0/screens/reservation/bikedetails_screen.dart';
 import 'package:bikeapp_v0/utils/config.dart';
+import 'package:bikeapp_v0/utils/next_screen.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
@@ -10,35 +12,38 @@ class CardWidget extends StatelessWidget {
     return Padding(
       padding: const EdgeInsets.all(10.0),
       child: GridView.builder(
-        itemCount: 10,
+        itemCount: 4,
         gridDelegate:
             SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2),
         itemBuilder: (context, index) {
           return Stack(//alignment: AlignmentDirectional.center,
               children: <Widget>[
-            Card(
-              shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(48)),
-              child: Column(
-                children: [
-                  Image.asset(
-                    'assets/bikeAppLogo.png',
-                    width: 150,
-                    height: 110,
-                  ),
-                  Text(
-                    'VTT ST 50',
-                    style: TextStyle(),
-                  ),
-                  Text('1 Dhs/Km'),
-                ],
+            GestureDetector(
+              onTap: () => nextScreen(context, BikeDetailsScreen()),
+              child: Card(
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(48)),
+                child: Column(
+                  children: [
+                    Image.asset(
+                      Config.app_icon,
+                      width: 150,
+                      height: 110,
+                    ),
+                    Text(
+                      'Umbro',
+                      style: TextStyle(),
+                    ),
+                    Text('1 Dhs/Km'),
+                  ],
+                ),
               ),
             ),
             Positioned(
               top: 0.0,
               child: SizedBox(
                 height: 30,
-                width: 58,
+                width: 64,
                 
                 child: TextButton(
                   style: TextButton.styleFrom(
